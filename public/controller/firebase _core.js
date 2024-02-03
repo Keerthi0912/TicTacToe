45bdda02f9536093bdc0414091a416ddc0dc6f6d
Keerthi0912 <kvanaparthi@uco.edu>
2024-02-02 18:19:46 -0800

    initial commit

---
+++ public/controller/firebase _core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDK8CSejFezFUC6xf1xg10pwKoUHjHNaqQ",
+    authDomain: "kvanaparthi-cmsc5373-webapps.firebaseapp.com",
+    projectId: "kvanaparthi-cmsc5373-webapps",
+    storageBucket: "kvanaparthi-cmsc5373-webapps.appspot.com",
+    messagingSenderId: "1061692662951",
+    appId: "1:1061692662951:web:fc26d1c2659eb5afcae782"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
